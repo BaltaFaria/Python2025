ea4af940c472e922a56d3ff083bba02b8680f02d
Balta Faria <baltafaria@gmail.com>
2025-03-19 19:16:53 -0300

    suma puntos

--- Questions.py
+++ Questions.py
@@ -20,35 +20,36 @@ answers = [
     ),
     ("=", "==", "!=", "==="),
 ]
-#Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
-correct_answers_index = [1, 2, 0, 3, 1]
 
-#El usuario deberá contestar 3 preguntas
+correct_answers_index = [1, 2, 0, 3, 1]
+puntos = 0
+
 for i in range(3):
-    # Se selecciona una pregunta aleatoria
+    
     question_index = random.randint(0, len(questions) - 1)
 
-    # Se muestra la pregunta y las respuestas posibles
+    
     print(questions[question_index])
     for i, answer in enumerate(answers[question_index]):
         print(f"{i + 1}. {answer}")
 
-    # El usuario tiene 2 intentos para responder correctamente
+   
     for intento in range(2):
         user_answer = int(input("Respuesta: ")) - 1
-        # Se verifica si la respuesta es correcta
+        
         #if user_answer != 1 or user_answer != 2 or user_answer != 3 or user_answer != 0:
         if user_answer not in [0, 1, 2, 3]:
-            print("respuesta incorrecta")
+            print("respuesta incorrecta")   
             exit(1)
         if user_answer == correct_answers_index[question_index]:
             print("¡Correcto!")
+            puntos += 1
             break
     else:
-        # Si el usuario no responde correctamente después de 2 intentos,
-        # se muestra la respuesta correcta
+       
         print("Incorrecto. La respuesta correcta es:")
         print(answers[question_index][correct_answers_index[question_index]])
-
-    # Se imprime un blanco al final de la pregunta
+        puntos -= 0.5
+    
     print()
+print("los puntos son: ",puntos)    
